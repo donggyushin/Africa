a1afa1eb207a44111c0f13b1cdaf4f82cb5dc529
신동규 <donggyu9410@gmail.com>
2024-08-20 21:42:08 +0900

    Gallery

---
+++ Gallery/Gallery.h
@@ -0,0 +1,18 @@
+//
+//  Gallery.h
+//  Gallery
+//
+//  Created by 신동규 on 8/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gallery.
+FOUNDATION_EXPORT double GalleryVersionNumber;
+
+//! Project version string for Gallery.
+FOUNDATION_EXPORT const unsigned char GalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gallery/PublicHeader.h>
+
+
