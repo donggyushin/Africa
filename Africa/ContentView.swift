d69e0979586a84beee339094d0510eda0cb53101
신동규 <donggyu9410@gmail.com>
2024-08-20 21:45:10 +0900

    Set Initial Screen

--- Africa/ContentView.swift
+++ Africa/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import Browse
+import Videos
+import Map
+import Gallery
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            BrowseView()
+                .tabItem {
+                    Image(systemName: "square.grid.2x2")
+                    Text("Browse")
+                }
+            
+            VideoListView()
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+            
+            BrowseView()
+                .tabItem {
+                    Image(systemName: "map")
+                    Text("Locations")
+                }
+            
+            BrowseView()
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("Gallery")
+                }
         }
-        .padding()
     }
 }
 
