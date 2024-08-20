0c5490d31f341c5a360c6998f1ce667e9da5d92f
신동규 <donggyu9410@gmail.com>
2024-08-20 21:22:44 +0900

    init project

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by 신동규 on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by 신동규 on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
