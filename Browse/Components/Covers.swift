7149f205e7cbf4fc7d80b3fe8cee0f36d26dd1bf
신동규 <donggyu9410@gmail.com>
2024-08-20 22:54:52 +0900

    Covers Component

---
+++ Browse/Components/Covers.swift
@@ -0,0 +1,34 @@
+//
+//  Covers.swift
+//  Browse
+//
+//  Created by 신동규 on 8/20/24.
+//
+
+import SwiftUI
+import Domain
+import Files
+
+struct Covers: View {
+    
+    let images: [CoverImageBrowse]
+    
+    var body: some View {
+        TabView {
+            ForEach(images) { image in
+                VStack {
+                    Image(image.name, bundle: .init(identifier: "com.donggyu.Browse"))
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+#Preview {
+    return Covers(images: COVER_IMAGES.map({ .init(domain: $0) }))
+        .previewLayout(.fixed(width: 400, height: 300))
+        .preferredColorScheme(.dark)
+}
