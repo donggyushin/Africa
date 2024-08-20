a0812245d2bd9b25b26f6cefae6e7a3139fdf9fc
신동규 <donggyu9410@gmail.com>
2024-08-20 21:27:34 +0900

    Add files

---
+++ Files/Files.h
@@ -0,0 +1,18 @@
+//
+//  Files.h
+//  Files
+//
+//  Created by 신동규 on 8/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Files.
+FOUNDATION_EXPORT double FilesVersionNumber;
+
+//! Project version string for Files.
+FOUNDATION_EXPORT const unsigned char FilesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Files/PublicHeader.h>
+
+
