cdfa0f985f6653ffbd5f9a84afdd562c7817c39a
신동규 <donggyu9410@gmail.com>
2024-08-20 21:38:04 +0900

    Browse Framework

---
+++ Browse/Browse.h
@@ -0,0 +1,18 @@
+//
+//  Browse.h
+//  Browse
+//
+//  Created by 신동규 on 8/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Browse.
+FOUNDATION_EXPORT double BrowseVersionNumber;
+
+//! Project version string for Browse.
+FOUNDATION_EXPORT const unsigned char BrowseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Browse/PublicHeader.h>
+
+
---
+++ Browse/Screen/BrowseView.swift
@@ -0,0 +1,23 @@
+//
+//  BrowseView.swift
+//  Browse
+//
+//  Created by 신동규 on 8/20/24.
+//
+
+import SwiftUI
+
+public struct BrowseView: View {
+    
+    public init() {
+        
+    }
+    
+    public var body: some View {
+        Text("Browse")
+    }
+}
+
+#Preview {
+    BrowseView()
+}
