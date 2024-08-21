06b6bd45cf6ef65b61d6201d9926bfc10a8b6ff0
신동규 <donggyu9410@gmail.com>
2024-08-21 22:47:29 +0900

    Animal Domain

---
+++ Domain/Model/AnimalDomain.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalDomain.swift
+//  Domain
+//
+//  Created by 신동규 on 8/21/24.
+//
+
+public struct AnimalDomain {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
