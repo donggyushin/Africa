a68f86dad4e280f12dff1bc171ede51f5e21f1bb
신동규 <donggyu9410@gmail.com>
2024-08-22 12:25:18 +0900

    detail

--- Browse/Screen/AnimalDetailView.swift
+++ Browse/Screen/AnimalDetailView.swift
@@ -22,7 +22,41 @@ public struct AnimalDetailView: View {
     }
     
     public var body: some View {
-        Text("Animal Detail")
+        ScrollView {
+            Image(animal.image, bundle: .init(identifier: "com.donggyu.Browse"))
+                .resizable()
+                .scaledToFit()
+            
+            Text(animal.name.uppercased())
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .padding(.vertical)
+                .fontWeight(.heavy)
+                .background {
+                    Color.accent
+                        .frame(height: 6)
+                        .offset(y: 24)
+                }
+            
+            Text(animal.headline)
+                .font(.headline)
+                .multilineTextAlignment(.leading)
+                .foregroundStyle(.accent)
+                .padding(.horizontal)
+            
+            HStack {
+                Image(systemName: "photo.on.rectangle.angled")
+                    .foregroundStyle(.accent)
+                    .imageScale(.large)
+                
+                Text("Wilderness in Pictures")
+                    .font(.title3)
+                    .fontWeight(.bold)
+            }
+            .padding(.vertical)
+        }
+        .scrollIndicators(.hidden)
+        .navigationTitle("Learn about \(animal.name)")
     }
 }
 
