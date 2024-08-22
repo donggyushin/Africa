cce57d5fc2e301eca57202a7be7eabdff9462626
신동규 <donggyu9410@gmail.com>
2024-08-22 17:52:07 +0900

    feat

--- Gallery/Model/AnimalGallery.swift
+++ Gallery/Model/AnimalGallery.swift
@@ -11,13 +11,11 @@ struct AnimalGallery: Identifiable {
     let id: String
     let name: String
     let image: String
-    let gallery: [String]
     
     init(domain: AnimalDomain) {
         id = domain.id
         name = domain.name
         image = domain.image
-        gallery = domain.gallery
     }
 }
 
