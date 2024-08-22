be2d34e16576bdf6a73955d4c0cc88d9a5d4537b
신동규 <donggyu9410@gmail.com>
2024-08-22 17:10:23 +0900

    Grid Animation

--- Gallery/Screen/GalleryView.swift
+++ Gallery/Screen/GalleryView.swift
@@ -13,12 +13,83 @@ public struct GalleryView: View {
     
     @StateObject var viewModel: GalleryViewModel
     
+    @State private var gridLayout: [GridItem] = [
+        .init(.flexible()),
+        .init(.flexible()),
+        .init(.flexible())
+    ]
+    
     public init(animalRepository: AnimalRepository) {
         _viewModel = .init(wrappedValue: .init(animalRepository: animalRepository))
     }
     
     public var body: some View {
-        Text("Gallery")
+        ScrollView {
+            GroupBox {
+                HStack {
+                    Button {
+                        withAnimation {
+                            gridLayout = [
+                                .init(.flexible()),
+                                .init(.flexible()),
+                            ]
+                        }
+                    } label: {
+                        HStack {
+                            Text("2")
+                            Image(systemName: "list.bullet")
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    Button {
+                        withAnimation {
+                            gridLayout = [
+                                .init(.flexible()),
+                                .init(.flexible()),
+                                .init(.flexible()),
+                            ]
+                        }
+                    } label: {
+                        HStack {
+                            Text("3")
+                            Image(systemName: "list.bullet")
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    Button {
+                        withAnimation {
+                            gridLayout = [
+                                .init(.flexible()),
+                                .init(.flexible()),
+                                .init(.flexible()),
+                                .init(.flexible()),
+                            ]
+                        }
+                    } label: {
+                        HStack {
+                            Text("4")
+                            Image(systemName: "list.bullet")
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .padding()
+            .foregroundStyle(.accent)
+            
+            LazyVGrid(columns: gridLayout, spacing: 10) {
+                ForEach(viewModel.animals) { animal in
+                    Image(animal.image, bundle: .init(identifier: "com.donggyu.Gallery"))
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .background {
+                            Circle().stroke(lineWidth: 2)
+                        }
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
     }
 }
 
