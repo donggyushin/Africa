9251c10d2a1db1a56eda59900b97bf89b93e9090
신동규 <donggyu9410@gmail.com>
2024-08-25 15:55:29 +0900

    replace

--- Browse/Screen/BrowseView.swift
+++ Browse/Screen/BrowseView.swift
@@ -19,21 +19,10 @@ public struct BrowseView: View {
     }
     
     public var body: some View {
-        ScrollView {
-            Covers(images: viewModel.covers)
-                .frame(height: 300)
-            
-            
-            ForEach(viewModel.animals) { data in
-                Button {
-                    Common.openURL(url: "dgafrica://browse?screen=detail&id=\(data.id)")
-                } label: {
-                    AnimalListItem(data: data)
-                        .foregroundStyle(Color(uiColor: .label))
-                }
-            }
-        }
-        .scrollIndicators(.hidden)
+        AnimalList(
+            covers: viewModel.covers,
+            animals: viewModel.animals
+        )
     }
 }
 
